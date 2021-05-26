0be682bba7ec92ac169272570b8b82d701c0180d
Justin Marin <jumarin@protonmail.com>
2021-05-26 14:58:08 -0400

    Cleaned up some sloppy copy/paste onthe HTML elements

--- src/App.jsx
+++ src/App.jsx
@@ -28,49 +28,46 @@ export class App extends Component {
   }
 
   render() {
-    const newBG = `hsl(${this.state.hue}, ${this.state.sat}%, ${this.state.light}%)`
-    const newStyle = { backgroundColor: newBG }
+    const newBackgroundColor = `hsl(${this.state.hue}, ${this.state.sat}%, ${this.state.light}%)`
+    const newStyle = { backgroundColor: newBackgroundColor }
     return (
       <div>
         <h1>Color Picker</h1>
         <div className="colorBox" style={newStyle}></div>
-        <p>{newBG}</p>
+        <p>{newBackgroundColor}</p>
         <div id="hSlider">
           <input
             type="range"
             min="1"
             max="360"
-            id="H"
             value={this.state.hue}
             onChange={this.updateHue}
           />
-          <label htmlFor="H">H</label>
+          <label>H</label>
         </div>
-        <div className="hSlider">
+        <div className="sSlider">
           <input
             type="range"
             min="1"
             max="100"
-            id="S"
             value={this.state.sat}
             onChange={this.updateSat}
           />
-          <label htmlFor="H">S</label>
+          <label>S</label>
         </div>
-        <div className="hSlider">
+        <div className="lSlider">
           <input
             type="range"
             min="1"
             max="100"
-            id="L"
             value={this.state.light}
             onChange={this.updateLight}
           />
-          <label htmlFor="H">L</label>
+          <label>L</label>
         </div>
-        <div className="hSlider">
-          <input type="range" min="1" max="100" id="A" />
-          <label htmlFor="H">A</label>
+        <div className="aSlider">
+          <input type="range" min="1" max="100" />
+          <label>A</label>
         </div>
         <button type="button" onClick={this.handleNewState}>
           Randomize!
